dcaed69f5ad1469e987e43a3de5e07b9f9d5d16d
mkotlyar <kotlyar.michael@gmail.com>
2013-11-28 18:23:07 +0200

    Next iteration

--- admin/src/main/java/com/rcg/admin/runner/JettyRunner.java
+++ admin/src/main/java/com/rcg/admin/runner/JettyRunner.java
@@ -4,8 +4,11 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyRunner {
+	
+	public static final int PORT = 8081;
+	
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8081);
+		Server server = new Server(PORT);
 		WebAppContext context = new WebAppContext();
 		context.setDescriptor("src/main/webapp/WEB-INF/web.xml");
 		context.setResourceBase("src/main/webapp");
