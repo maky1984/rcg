0eacea52ed515078fd632233873a7faa69617e25
mkotlyar <kotlyar.michael@gmail.com>
2014-03-24 19:36:51 +0200

    Next iteration

---
+++ common/src/main/java/com/rcg/common/RequestGameTableUpdate.java
@@ -0,0 +1,8 @@
+package com.rcg.common;
+
+public class RequestGameTableUpdate extends ClientRequest {
+
+	public RequestGameTableUpdate() {
+		super(RequestGameTableUpdate.class.getName());
+	}
+}
---
+++ common/src/main/java/com/rcg/game/model/RuleConstants.java
@@ -0,0 +1,7 @@
+package com.rcg.game.model;
+
+public class RuleConstants {
+
+	public static final int MAX_HAND_CARD_NUMBER = 6;
+	
+}
