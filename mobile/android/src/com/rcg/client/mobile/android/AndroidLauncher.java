5ab64689de1eace3d51a9f50fcca25bddb0b81e3
mkotlyar <kotlyar.michael@gmail.com>
2014-05-12 15:32:05 +0300

    Next iteration

---
+++ mobile/android/src/com/rcg/client/mobile/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rcg.client.mobile.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rcg.client.mobile.RCGClientMobile;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RCGClientMobile(), config);
+	}
+}
---
+++ mobile/desktop/src/com/rcg/client/mobile/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rcg.client.mobile.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rcg.client.mobile.RCGClientMobile;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RCGClientMobile(), config);
+	}
+}
